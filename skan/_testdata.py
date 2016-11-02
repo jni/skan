b4867118f3927b3a70ade44a547aeea1971be26a
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-11-02 19:30:21 +1100

    Add new tests

--- skan/_testdata.py
+++ skan/_testdata.py
@@ -11,3 +11,8 @@ skeleton1 = np.array([[0, 1, 1, 1, 1, 1, 0],
                       [0, 1, 1, 0, 1, 1, 0],
                       [1, 0, 0, 1, 0, 0, 0],
                       [1, 0, 0, 0, 1, 1, 1]], dtype=bool)
+
+
+zeros1 = np.zeros_like(skeleton1)
+skeleton2 = np.column_stack((skeleton1, zeros1))
+skeleton2 = np.row_stack((skeleton2, skeleton2[:, ::-1]))
--- skan/test/test_csr.py
+++ skan/test/test_csr.py
@@ -6,7 +6,7 @@ from skan import csr
 rundir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(rundir)
 
-from skan._testdata import tinycycle, skeleton1
+from skan._testdata import tinycycle, skeleton1, skeleton2
 
 
 def test_tiny_cycle():
@@ -27,4 +27,23 @@ def test_tiny_cycle():
 def test_skeleton1_stats():
     args = csr.skeleton_to_csgraph(skeleton1)
     stats = csr.branch_statistics(*args)
+    assert_equal(stats.shape, (4, 4))
+    keys = map(tuple, stats[:, :2].astype(int))
+    dists = stats[:, 2]
+    types = stats[:, 3].astype(int)
+    ids2dist = dict(zip(keys, dists))
+    assert (13, 8) in ids2dist
+    assert (8, 13) in ids2dist
+    d0, d1 = sorted((ids2dist[(13, 8)], ids2dist[(8, 13)]))
+    assert_almost_equal(d0, 1 + np.sqrt(2))
+    assert_almost_equal(d1, 5*d1)
+    assert_equal(np.bincount(types), [0, 2, 2])
+    assert_almost_equal(np.unique(dists), [d0, 2 + np.sqrt(2), d1])
 
+
+def test_3skeletons():
+    df = csr.summarise(skeleton2)
+    assert_almost_equal(np.unique(df['euclidean-distance']),
+                        np.sqrt([5, 10]))
+    assert_equal(np.unique(df['skeleton-id']), [1, 2])
+    assert_equal(np.bincount(df['branch-type']), [0, 4, 4])
