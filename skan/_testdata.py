b282434e61cd1a185806b985953d1543a57bca8f
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-11-02 18:00:14 +1100

    Add some tiny synthetic data for testing

---
+++ skan/_testdata.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+tinycycle = np.array([[0, 1, 0],
+                      [1, 0, 1],
+                      [0, 1, 0]], dtype=bool)
+
+
+skeleton1 = np.array([[0, 1, 1, 1, 1, 1, 0],
+                      [1, 0, 0, 0, 0, 0, 1],
+                      [0, 1, 1, 0, 1, 1, 0],
+                      [1, 0, 0, 1, 0, 0, 0],
+                      [1, 0, 0, 0, 1, 1, 1]], dtype=bool)
