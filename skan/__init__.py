8523a1726ac269452677669b9b31c476cfeee5f8
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-11-08 13:52:53 +1100

    Bump version: 0.1-dev → 0.1

--- skan/__init__.py
+++ skan/__init__.py
@@ -1,6 +1,6 @@
 from .csr import skeleton_to_csgraph, branch_statistics, summarise
 
-__version__ = '0.1-dev'
+__version__ = '0.1'
 
 __all__ = ['skeleton_to_csgraph',
            'branch_statistics',
