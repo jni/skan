1b7c1c30e910a0a02b97fc0b691b9f33136ae8d4
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-12-21 12:17:45 +1100

    Add automated population of GUI rows

--- skan/gui.py
+++ skan/gui.py
@@ -9,11 +9,19 @@ class Launch(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Skeleton analysis tool')
-        self.smooth_radius = tk.DoubleVar(value=0.1, name='foo')
-        self.threshold_radius = tk.DoubleVar(value=50e-9, name='bar')
-        self.brightness_offset = tk.DoubleVar(value=-10, name='baz')
+        self.smooth_radius = tk.DoubleVar(value=0.1, name='Smoothing radius')
+        self.threshold_radius = tk.DoubleVar(value=50e-9,
+                                             name='Threshold radius')
+        self.brightness_offset = tk.DoubleVar(value=-10,
+                                              name='Brightness offset')
         self.scale_metadata_path = tk.StringVar(value='Scan,PixelHeight',
-                                                name='bork')
+                                                name='Scale metadata path')
+        self.parameters = [
+            self.smooth_radius,
+            self.threshold_radius,
+            self.brightness_offset,
+            self.scale_metadata_path
+        ]
 
         # allow resizing
         self.rowconfigure(0, weight=1)
@@ -33,23 +41,12 @@ class Launch(tk.Tk):
         heading.grid(column=0, row=cur_row, sticky='n')
         cur_row += 1
 
-        smooth_label = ttk.Label(parameters, text='Smoothing radius')
-        smooth_label.grid(column=0, row=cur_row, sticky='nsew')
-        smooth = ttk.Entry(parameters, textvariable=self.smooth_radius)
-        smooth.grid(column=1, row=cur_row, sticky='nsew')
-        cur_row += 1
-
-        threshold_label = ttk.Label(parameters, text='Threshold radius')
-        threshold_label.grid(row=cur_row, column=0, sticky='nsew')
-        threshold = ttk.Entry(parameters, textvariable=self.threshold_radius)
-        threshold.grid(row=cur_row, column=1, sticky='nsew')
-        cur_row += 1
-
-        brightness_label = ttk.Label(parameters, text='Brightness offset')
-        brightness_label.grid(row=cur_row, column=0, sticky='nsew')
-        brightness = ttk.Entry(parameters, textvariable=self.brightness_offset)
-        brightness.grid(row=cur_row, column=1, sticky='nsew')
-        cur_row += 1
+        for i, param in enumerate(self.parameters):
+            param_label = ttk.Label(parameters, text=param._name)
+            param_label.grid(row=cur_row + i, column=0, sticky='nsew')
+            param_entry = ttk.Entry(parameters, textvariable=param)
+            param_entry.grid(row=cur_row + i, column=1, sticky='nsew')
+        cur_row += i
 
         main.pack()
 
