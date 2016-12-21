f12760812c145afd0fa2a90fff621422c3193c5f
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-12-21 15:46:50 +1100

    Add image loading and format support

--- skan/gui.py
+++ skan/gui.py
@@ -3,6 +3,12 @@ import tkinter as tk
 import tkinter.filedialog
 from tkinter import ttk
 
+from tqdm import tqdm
+import imageio
+import numpy as np
+from skimage import morphology
+import pandas as pd
+
 
 STANDARD_MARGIN = (3, 3, 12, 12)
 
@@ -16,12 +22,15 @@ class Launch(tk.Tk):
                                              name='Threshold radius')
         self.brightness_offset = tk.DoubleVar(value=-10,
                                               name='Brightness offset')
+        self.image_format = tk.StringVar(value='auto',
+                                         name='Image format')
         self.scale_metadata_path = tk.StringVar(value='Scan,PixelHeight',
                                                 name='Scale metadata path')
         self.parameters = [
             self.smooth_radius,
             self.threshold_radius,
             self.brightness_offset,
+            self.image_format,
             self.scale_metadata_path
         ]
 
@@ -75,10 +84,44 @@ class Launch(tk.Tk):
                 tk.filedialog.askdirectory(initialdir=self.output_folder)
 
     def run(self):
+        print('Input files:')
+        for file in self.input_files:
+            print('  ', file)
+        print('Parameters:')
+        for param in self.parameters:
+            p = param.get()
+            print('  ', param, type(p), p)
+        print('Output:', self.output_folder)
+        image_format = (None if self.image_format == 'auto'
+                        else self.image_format)
+        results = []
         from skan import pre, csr
-        print('In:', self.input_files)
-        print('Out:', self.output_folder)
-        print('running!')
+        for file in tqdm(self.input_files):
+            image = imageio.imread(file, format=image_format)
+            if self.scale_metadata_path is not None:
+                md_path = str.split(self.scale_metadata_path, sep=',')
+                meta = image.meta
+                for key in md_path:
+                    meta = meta[key]
+                scale = float(meta)
+            else:
+                scale = 1  # measurements will be in pixel units
+            pixel_threshold_radius = int(np.ceil(self.threshold_radius.get() /
+                                                 scale))
+            pixel_smoothing_radius = (self.smooth_radius.get() *
+                                      pixel_threshold_radius)
+            thresholded = pre.threshold(image, sigma=pixel_smoothing_radius,
+                                        radius=pixel_threshold_radius,
+                                        offset=self.brightness_offset.get())
+            skeleton = morphology.skeletonize(thresholded)
+            framedata = csr.summarise(skeleton, spacing=scale)
+            framedata['squiggle'] = np.log2(framedata['branch-distance'] /
+                                            framedata['euclidean-distance'])
+            framedata['filename'] = [file] * len(framedata)
+            results.append(framedata)
+        results = pd.concat(results)
+        results.to_csv(os.path.join(self.output_folder, 'skeletons.csv'))
+
 
 if __name__ == '__main__':
     app = Launch()
