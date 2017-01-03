e379b89ab5f012ac9ad6f6f0a058fbe3098791a6
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-01-03 11:40:27 +1100

    Add docstring to pipe.process_images

--- skan/pipe.py
+++ skan/pipe.py
@@ -8,6 +8,34 @@ import pandas as pd
 
 def process_images(filenames, image_format, threshold_radius,
                    smooth_radius, brightness_offset, scale_metadata_path):
+    """Full pipeline from images to skeleton stats with local median threshold.
+
+    Parameters
+    ----------
+    filenames : list of string
+        The list of input filenames.
+    image_format : string
+        The format of the files. 'auto' is automatically determined by the
+        imageio library. See imageio documentation for valid image formats.
+    threshold_radius : float
+        The radius for median thresholding,
+    smooth_radius : float in [0, 1]
+        The value of sigma with which to Gaussian-smooth the image,
+        **relative to `threshold_radius`**.
+    brightness_offset : float
+        The standard brightness value with which to threshold is the local
+        median, `m(x, y)`. Use this value to offset from there: the threshold
+        used will be `m(x, y) + brightness_offset`.
+    scale_metadata_path : string
+        The path in the image dictionary to find the metadata on pixel scale,
+        separated by forward slashes ('/').
+
+    Returns
+    -------
+    result : pandas DataFrame
+        Data frame containing all computed statistics on the skeletons found
+        in the input image.
+    """
     image_format = None if image_format == 'auto' else image_format
     results = []
     for file in tqdm(filenames):
