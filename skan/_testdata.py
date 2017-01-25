597f51859a6f0e5e0c9d368aa630d79800711372
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-01-25 19:11:43 +1100

    Add test skeleton w/ junction of multiplicity > 1

--- skan/_testdata.py
+++ skan/_testdata.py
@@ -9,6 +9,11 @@ tinycycle = np.array([[0, 1, 0],
 tinyline = np.array([0, 1, 1, 1, 0], dtype=bool)
 
 
+skeleton0 = np.array([[0, 0, 1, 0, 0],
+                      [0, 0, 1, 0, 0],
+                      [1, 1, 1, 1, 1]], dtype=bool)
+
+
 skeleton1 = np.array([[0, 1, 1, 1, 1, 1, 0],
                       [1, 0, 0, 0, 0, 0, 1],
                       [0, 1, 1, 0, 1, 1, 0],
