676568d172a84afeb376b6ca80225af8aae6af96
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-02-07 11:40:19 +1100

    Bump version: 0.4.0-dev → 0.5.0-dev

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ MAINTAINER_EMAIL    = 'juan.n@unimelb.edu.au'
 URL                 = 'https://github.com/jni/skan'
 LICENSE             = 'BSD 3-clause'
 DOWNLOAD_URL        = 'https://github.com/jni/skan'
-VERSION             = '0.4.0-dev'
+VERSION             = '0.5.0-dev'
 PYTHON_VERSION      = (3, 5)
 INST_DEPENDENCIES   = {} 
 
