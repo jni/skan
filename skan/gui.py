a93c05c4f51b2beb70eb30f042423d8b511ec66d
Adam Blanch <ablanch@unimelb.edu.au>
2017-05-23 11:04:48 +1000

    filter and compile by filename

--- skan/gui.py
+++ skan/gui.py
@@ -243,12 +243,15 @@ class Launch(tk.Tk):
             else:
                 result_full, result_image = result
                 result_filtered = result_full[(result_full['mean shape index']>0.125) &
-                                              (result_full['mean shape index']<0.625)]
+                                              (result_full['mean shape index']<0.625) &
+                                              (result_full['branch-type'] == 2) 
+                                              (result_full['euclidean-distance']>0)]
+                ridgeydata = result_filtered.groupby('filename')[['filename','branch-distance','scale','euclidean-distance','squiggle','mean shape index']].mean()
                 io.write_excel(str(self.output_folder /
                                    self.output_filename.get()),
                                branches=result_full,
                                images=result_image,
-                               filtered=result_filtered,
+                               filtered=ridgeydata,
                                parameters=json.loads(self.save_parameters()))
 
 
