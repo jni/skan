e6b708f62bc2fd92157918d74dffed8d56c91d8a
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-11-21 03:38:28 -0600

    Update version to v0.10.0rc2

--- src/skan/__init__.py
+++ src/skan/__init__.py
@@ -1,5 +1,5 @@
 from .csr import skeleton_to_csgraph, branch_statistics, summarize, Skeleton
 
-__version__ = '0.10.0rc1'
+__version__ = '0.10.0rc2'
 
 __all__ = ['skeleton_to_csgraph', 'branch_statistics', 'summarize', 'Skeleton']
