b3e7d06319098884b988e3d120b04473eeeabd6e
James Ryan <work.jdryan@gmail.com>
2022-12-22 12:59:27 +1100

    initial skeletonize widget

--- src/napari_skan/napari_skan.py
+++ src/napari_skan/napari_skan.py
@@ -1,2 +1,13 @@
-def hello_world():
-    print("Hello, world!")
+import numpy as np
+from enum import Enum
+from skimage.morphology import skeletonize
+
+class SkeletonizeMethod(Enum):
+    Zhang = "zhang"
+    Lee = "lee"
+
+
+def skeletonize_labels(labels: "napari.types.LabelsData", method: SkeletonizeMethod) -> "napari.types.LabelsData":
+    binary_labels = (labels > 0).astype(np.uint8)
+    skeletonized = skeletonize(binary_labels, method=method)
+    return skeletonized
