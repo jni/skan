4a29c2f550b5fbbcbf1b40f8e6d550346fb2cea1
James Ryan <work.jdryan@gmail.com>
2023-01-11 12:17:23 +1100

    added documentaiton to widget

--- src/skan/napari_skan.py
+++ src/skan/napari_skan.py
@@ -8,6 +8,20 @@ class SkeletonizeMethod(Enum):
 
 
 def skeletonize_labels(labels: "napari.types.LabelsData", method: SkeletonizeMethod) -> "napari.types.LabelsData":
+    """Takes a labels layer and a skimage skeletonize method and generates a skeleton representation
+
+    Parameters
+    ----------
+    labels : napari.types.LabelsData
+        A labels layer containing data to skeletonize
+    method : SkeletonizeMethod
+        Enum denoting the chosen skeletonize method method
+
+    Returns
+    -------
+    napari.types.LabelsData
+        Labels layer depecting the extracted skeleton
+    """
     binary_labels = (labels > 0).astype(np.uint8)
     skeletonized = skeletonize(binary_labels, method=method.value)
     return skeletonized
