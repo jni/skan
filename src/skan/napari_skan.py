8505544a41411ce55407096b6f9ea587c46baa31
jamesyan-git <work.jdryan@gmail.com>
2023-02-28 13:13:16 +1100

    chenged result to match labels colour

--- src/skan/napari_skan.py
+++ src/skan/napari_skan.py
@@ -23,5 +23,6 @@ def skeletonize_labels(labels: "napari.types.LabelsData", method: SkeletonizeMet
         Labels layer depecting the extracted skeleton
     """
     binary_labels = (labels > 0).astype(np.uint8)
-    skeletonized = skeletonize(binary_labels, method=method.value)
+    # skeletonize returns a binary array, so we can just iltiply it with the labels to get appropriate colours
+    skeletonized = skeletonize(binary_labels, method=method.value) * labels
     return skeletonized
