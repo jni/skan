033d4fdd72e00574c0deb692d0d4a3b0f2b06f8d
jamesyan-git <work.jdryan@gmail.com>
2023-05-17 11:59:52 +1000

    refactored Skeletonize to magic gui

--- src/skan/napari_skan.py
+++ src/skan/napari_skan.py
@@ -3,107 +3,26 @@ from enum import Enum
 from skimage.morphology import skeletonize
 from skan import summarize, Skeleton
 import napari
-from magicgui.widgets import Container, ComboBox, PushButton, Label
-
+from magicgui.widgets import Container, ComboBox, PushButton, Label, create_widget
+from napari.layers import Shapes 
 class SkeletonizeMethod(Enum):
     zhang = "zhang"
     lee = "lee"
 
-
-def skeletonize_labels(labels: "napari.types.LabelsData", method: SkeletonizeMethod) -> "napari.types.LabelsData":
-    """Takes a labels layer and a skimage skeletonize method and generates a skeleton representation
-
-    Parameters
-    ----------
-    labels : napari.types.LabelsData
-        A labels layer containing data to skeletonize
-    method : SkeletonizeMethod
-        Enum denoting the chosen skeletonize method method
-
-    Returns
-    -------
-    napari.types.LabelsData
-        Labels layer depecting the extracted skeleton
-    """
-    binary_labels = (labels > 0).astype(np.uint8)
-
-    skeletonized = skeletonize(binary_labels, method=method.value) * labels
-    return skeletonized
-
-
-def analyze_skeleton(labels: "napari.types.LabelsData"):
-    binary_labels = (labels > 0).astype(np.uint8)
-    binary_skeleton = skeletonize(binary_labels)
+def get_skeleton(labels: "napari.layers.Labels", choice: SkeletonizeMethod) -> "napari.layers.Shapes":
+    binary_labels = (labels.data > 0).astype(np.uint8)
+    binary_skeleton = skeletonize(binary_labels, method=choice.value)
     
-    skeleton = skeletonize(binary_skeleton)
-    paths_table = summarize(skeleton)
-    color_options = paths_table.features
-    
-class SkeletonizeWidget(Container):
-    def __init__(self, viewer: "napari.viewer.Viewer"):
-        """Widget for skeletonizing labels layer. 
+    skeleton = Skeleton(binary_skeleton)
 
-        Parameters 
-        ----------
-        viewer : napari.viewer.Viewer
-            napari viewer to add the widget to
-        """
-        super().__init__()
-        self.viewer = viewer
-        self.current_label_layer = None
-        self.labels_combo = ComboBox(
-                name='Labels Layer', choices=self.get_labels_layers
-                )
-        self.methods_combo = ComboBox(
-                name="Method", choices = SkeletonizeMethod
+    all_paths = [skeleton.path_coordinates(i)  
+            for i in range(skeleton.n_paths)]
+
+    return Shapes(all_paths,
+        shape_type='path',
+        edge_colormap='tab10',
+        metadata={"skeleton": skeleton}
         )
-        self.skeletonize_button = PushButton(name='Skeletonize')
-        self.skeletonize_button.clicked.connect(self.make_skeleton_layer)
-        
-        self.extend([self.labels_combo, self.methods_combo, self.skeletonize_button])
-
-    def make_skeleton_layer(self):
-        layer_name = self.labels_combo.current_choice
-        self.current_label_layer = self.viewer.layers[layer_name].data
-        binary_labels = (self.current_label_layer > 0).astype(np.uint8)
-        binary_skeleton = skeletonize(binary_labels, method=self.methods_combo.current_choice)
-        
-        skeleton = Skeleton(binary_skeleton)
-
-        all_paths = [skeleton.path_coordinates(i)  
-                for i in range(skeleton.n_paths)]
-        self.skeleton_layer = self.viewer.add_shapes(
-            all_paths,
-            shape_type='path',
-            # features=paths_table,
-            # edge_width=0.5,
-            # edge_color='skeleton-id',
-            edge_colormap='tab10',
-            metadata={"skeleton": skeleton}
-        )
-
-    def set_current_layer(self):
-        #TODO: error test
-        self.current_label_layer = self.viewer.layers[self.labels_combo.current_choice].data
-
-    def get_labels_layers(self, combo):
-        """Returns a list of existing labels to display 
-
-        Parameters
-        ----------
-        combo : magicgui ComboBox
-            A dropdown to dispaly the layers
-
-        Returns
-        -------
-        list[napari.layer.label]
-            A list of curently existing layers
-        """
-        return [
-                layer for layer in self.viewer.layers
-                if isinstance(layer, napari.layers.Labels)
-                ]
-
 
 class AnalyseSkeleton(Container):
     def __init__(self, viewer: "napari.viewer.Viewer"):
@@ -152,7 +71,7 @@ class AnalyseSkeleton(Container):
                 ]
 
     def update_edge_color(self, value):
-        """update the color of the currently selected shapes layer"""        
+        """update the color of the currently selected shapes layer """        
         self.viewer.layers[self.shapes_combo.current_choice].edge_color = value
 
 if __name__ == "__main__":
