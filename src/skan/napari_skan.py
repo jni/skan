5ca99dab65354dd16b3adca5d19ed91bc0fa05e6
jamesyan-git <work.jdryan@gmail.com>
2023-06-16 13:09:05 +1000

    removed old widget

--- src/skan/napari_skan.py
+++ src/skan/napari_skan.py
@@ -23,7 +23,6 @@ def get_skeleton(labels: "napari.layers.Labels", choice: SkeletonizeMethod) -> "
     
     paths_table = summarize(skeleton) # option to have main_path = True (or something) changing header
 
-
     return (
         all_paths,
         {'shape_type': 'path', 'edge_colormap': 'tab10', 'metadata': {'skeleton': skeleton, 'features': paths_table}},
@@ -58,47 +57,6 @@ def color_by_feature(shapes_layer:"napari.layers.Shapes", feature_name):
         shapes_layer.edge_colormap = CAT_COLOR
     shapes_layer.edge_color = feature_name
 
-
-
-    
-class AnalyseSkeleton(Container):
-    def __init__(self, viewer: "napari.viewer.Viewer"):
-        """Widget for analyzing skeleton of an existing shapes layer 
-
-        Parameters 
-        ----------
-        viewer : napari.viewer.Viewer
-            napari viewer to add the widget to
-        """
-        super().__init__()
-        self.viewer = viewer
-        self.shapes_combo = create_widget(annotation = "napari.layers.Shapes", name = "skeleton")
-        # removed button, connected instead to Shapes changing to populate the features
-        self.shapes_combo.changed.connect(self._populate_features_combo)
-
-        self.features_combo = ComboBox(name='feature')
-        # added function call to populate combo
-        self._populate_features_combo(True)
-        self.extend([self.shapes_combo,self.features_combo])
-
-    def update_edge_color(self, value):
-        """update the color of the currently selected shapes layer """       
-        shapes_layer = self.viewer.layers[self.shapes_combo.current_choice]
-        current_column_type = shapes_layer.features[value].dtype
-        if current_column_type == "float64":
-            shapes_layer.edge_colormap = CONTINUOUS_COLOR
-        else:
-            shapes_layer.edge_colormap = CAT_COLOR
-        shapes_layer.edge_color = value
-
-    def _populate_features_combo(self, event):
-        print(self.shapes_combo.current_choice)
-        current_layer = self.viewer.layers[self.shapes_combo.current_choice]
-        current_layer.features = current_layer.metadata["features"]
-        self.features_combo.choices = current_layer.features.columns
-        # the choises exist at this point, but gui is not updating hence to labeled changed (hoping event emission)
-        self.features_combo.label_changed(current_layer.name)
-        self.features_combo.changed.connect(self.update_edge_color)
 
 if __name__ == "__main__":
     import napari
