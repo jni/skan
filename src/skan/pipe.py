Correcting string normalisation and restoring parenteses

src/skan/pipe.py
@@ -33,7 +33,7 @@ def _get_scale(image, md_path_or_scale):
     except ValueError:
         pass
     if md_path_or_scale is not None and scale is None:
-        md_path = md_path_or_scale.split(sep="/")
+        md_path = md_path_or_scale.split(sep='/')
         meta = image.meta
         for key in md_path:
             meta = meta[key]
@@ -45,14 +45,8 @@ def _get_scale(image, md_path_or_scale):
 
 
 def process_single_image(
-        filename,
-        image_format,
-        scale_metadata_path,
-        threshold_radius,
-        smooth_radius,
-        brightness_offset,
-        crop_radius,
-        smooth_method,
+        filename, image_format, scale_metadata_path, threshold_radius,
+        smooth_radius, brightness_offset, crop_radius, smooth_method,
         ):
     image = imageio.v2.imread(filename, format=image_format)
     scale = _get_scale(image, scale_metadata_path)
@@ -69,19 +63,19 @@ def process_single_image(
             offset=brightness_offset,
             smooth_method=smooth_method,
             )
-    quality = shape_index(image, sigma=pixel_smoothing_radius, mode="reflect")
+    quality = shape_index(image, sigma=pixel_smoothing_radius, mode='reflect')
     skeleton = morphology.skeletonize(thresholded) * quality
     framedata = csr.summarize(csr.Skeleton(skeleton, spacing=scale))
-    framedata["squiggle"] = np.log2(
-            framedata["branch_distance"] / framedata["euclidean_distance"]
+    framedata['squiggle'] = np.log2(
+            framedata['branch_distance'] / framedata['euclidean_distance']
             )
-    framedata["scale"] = scale
+    framedata['scale'] = scale
     framedata.rename(
-            columns={"mean_pixel_value": "mean_shape_index"},
+            columns={'mean_pixel_value': 'mean_shape_index'},
             inplace=True,
-            errors="raise",
+            errors='raise',
             )
-    framedata["filename"] = filename
+    framedata['filename'] = filename
     return image, thresholded, skeleton, framedata
 
 
@@ -140,15 +134,9 @@ def process_images(
     with ThreadPoolExecutor(max_workers=num_threads) as ex:
         future_data = {
                 ex.submit(
-                        process_single_image,
-                        filename,
-                        image_format,
-                        scale_metadata_path,
-                        threshold_radius,
-                        smooth_radius,
-                        brightness_offset,
-                        crop_radius,
-                        smooth_method,
+                        process_single_image, filename, image_format,
+                        scale_metadata_path, threshold_radius, smooth_radius,
+                        brightness_offset, crop_radius, smooth_method,
                         ): filename
                 for filename in filenames
                 }
@@ -158,14 +146,16 @@ def process_images(
             filename = future_data[completed_data]
             results.append(framedata)
             image_stats = image_summary(
-                    skeleton, spacing=framedata["scale"][0]
+                    skeleton, spacing=framedata['scale'][0]
                     )
-            image_stats["filename"] = filename
-            image_stats["branch density"
-                        ] = framedata.shape[0] / image_stats["area"]
-            j2j = framedata[framedata["branch_type"] == 2]
-            image_stats["mean J2J branch distance"] = j2j["branch_distance"
-                                                          ].mean()
+            image_stats['filename'] = filename
+            image_stats['branch density'] = (
+                    framedata.shape[0] / image_stats["area"]
+                    )
+            j2j = framedata[framedata['branch_type'] == 2]
+            image_stats['mean J2J branch distance'] = (
+                    j2j['branch_distance'].mean()
+                    )
             image_results.append(image_stats)
             yield filename, image, thresholded, skeleton, framedata
     yield pd.concat(results), pd.concat(image_results)

src/skan/summary_utils.py
@@ -20,9 +20,9 @@ def find_main_branches(summary: DataFrame) -> np.ndarray:
        skeleton
     """
     is_main = np.zeros(summary.shape[0], dtype=bool)
-    us = summary["node_id_src"]
-    vs = summary["node_id_dst"]
-    ws = summary["branch_distance"]
+    us = summary['node_id_src']
+    vs = summary['node_id_dst']
+    ws = summary['branch_distance']
 
     edge2idx = {(u, v): i for i, (u, v) in enumerate(zip(us, vs))}
 
@@ -46,7 +46,7 @@ def find_main_branches(summary: DataFrame) -> np.ndarray:
         for i, j in tz.sliding_window(2, nx.shortest_path(h,
                                                           source=curr_pair[0],
                                                           target=curr_pair[1],
-                                                          weight="weight")):
+                                                          weight='weight')):
             is_main[edge2idx[(i, j)]] = 1
 
     return is_main

src/skan/test/test_csr.py
@@ -6,7 +6,10 @@ from numpy.testing import assert_equal, assert_almost_equal
 from skimage.draw import line
 
 from skan import csr, summarize
-from skan._testdata import tinycycle, tinyline, skeleton0, skeleton1, skeleton2, skeleton3d, topograph1d, skeleton4
+from skan._testdata import (
+        tinycycle, tinyline, skeleton0, skeleton1, skeleton2, skeleton3d,
+        topograph1d, skeleton4
+        )
 
 
 def _old_branch_statistics(
@@ -16,7 +19,7 @@ def _old_branch_statistics(
             skeleton_image, spacing=spacing, value_is_height=value_is_height
             )
     summary = csr.summarize(skel, value_is_height=value_is_height)
-    columns = ["node_id_src", "node_id_dst", "branch_distance", "branch_type"]
+    columns = ['node_id_src', 'node_id_dst', 'branch_distance', 'branch_type']
     return summary[columns].to_numpy()
 
 
@@ -52,22 +55,22 @@ def test_skeleton1_stats():
 
 def test_2skeletons():
     df = csr.summarize(csr.Skeleton(skeleton2))
-    assert_almost_equal(np.unique(df["euclidean_distance"]), np.sqrt([5, 10]))
-    assert_equal(np.unique(df["skeleton_id"]), [0, 1])
-    assert_equal(np.bincount(df["branch_type"]), [0, 4, 4])
+    assert_almost_equal(np.unique(df['euclidean_distance']), np.sqrt([5, 10]))
+    assert_equal(np.unique(df['skeleton_id']), [0, 1])
+    assert_equal(np.bincount(df['branch_type']), [0, 4, 4])
 
 
 def test_summarize_spacing():
     df = csr.summarize(csr.Skeleton(skeleton2))
     df2 = csr.summarize(csr.Skeleton(skeleton2, spacing=2))
-    assert_equal(np.array(df["node_id_src"]), np.array(df2["node_id_src"]))
+    assert_equal(np.array(df['node_id_src']), np.array(df2['node_id_src']))
     assert_almost_equal(
-            np.array(df2["euclidean_distance"]),
-            np.array(2 * df["euclidean_distance"])
+            np.array(df2['euclidean_distance']),
+            np.array(2 * df['euclidean_distance'])
             )
     assert_almost_equal(
-            np.array(df2["branch_distance"]),
-            np.array(2 * df["branch_distance"])
+            np.array(df2['branch_distance']),
+            np.array(2 * df['branch_distance'])
             )
 
 
@@ -105,8 +108,8 @@ def test_topograph_summary():
             csr.Skeleton(topograph1d, spacing=2.5, value_is_height=True),
             value_is_height=True,
             )
-    assert stats.loc[0, "euclidean_distance"] == 5.0
-    columns = ["coord_src_0", "coord_src_1", "coord_dst_0", "coord_dst_1"]
+    assert stats.loc[0, 'euclidean_distance'] == 5.0
+    columns = ['coord_src_0', 'coord_src_1', 'coord_dst_0', 'coord_dst_1']
     assert_almost_equal(sorted(stats.loc[0, columns]), [0, 3, 3, 5])
 
 
@@ -121,12 +124,12 @@ def test_multiplicity_stats():
     stats1 = csr.summarize(csr.Skeleton(skeleton0))
     stats2 = csr.summarize(csr.Skeleton(skeleton0, spacing=2))
     assert_almost_equal(
-            2 * stats1["branch_distance"].values,
-            stats2["branch_distance"].values
+            2 * stats1['branch_distance'].values,
+            stats2['branch_distance'].values
             )
     assert_almost_equal(
-            2 * stats1["euclidean_distance"].values,
-            stats2["euclidean_distance"].values
+            2 * stats1['euclidean_distance'].values,
+            stats2['euclidean_distance'].values
             )
 
 
@@ -134,7 +137,7 @@ def test_pixel_values():
     image = np.random.random((45,))
     expected = np.mean(image)
     stats = csr.summarize(csr.Skeleton(image))
-    assert_almost_equal(stats.loc[0, "mean_pixel_value"], expected)
+    assert_almost_equal(stats.loc[0, 'mean_pixel_value'], expected)
 
 
 def test_tip_junction_edges():
@@ -177,7 +180,7 @@ def test_transpose_image():
 
 
 @pytest.mark.parametrize(
-        "skeleton,prune_branch,target",
+        'skeleton,prune_branch,target',
         [
                 (
                         skeleton1,
@@ -221,7 +224,7 @@ def test_prune_paths(
     """Test pruning of paths."""
     s = csr.Skeleton(skeleton, keep_images=True)
     summary = summarize(s)
-    indices_to_remove = summary.loc[summary["branch_type"] == prune_branch
+    indices_to_remove = summary.loc[summary['branch_type'] == prune_branch
                                     ].index
     pruned = s.prune_paths(indices_to_remove)
     np.testing.assert_array_equal(pruned, target)
@@ -232,7 +235,7 @@ def test_prune_paths_exception_single_point() -> None:
     can not be created and returned."""
     s = csr.Skeleton(skeleton0)
     summary = summarize(s)
-    indices_to_remove = summary.loc[summary["branch_type"] == 1].index
+    indices_to_remove = summary.loc[summary['branch_type'] == 1].index
     with pytest.raises(ValueError):
         s.prune_paths(indices_to_remove)
 

src/skan/test/test_napari_plugin.py
@@ -22,14 +22,14 @@ def test_get_skeleton_simple():
             labels_layer, skeleton_type
             )
 
-    assert type(layer_kwargs["metadata"]["skeleton"]) is Skeleton
+    assert type(layer_kwargs['metadata']['skeleton']) is Skeleton
     np.testing.assert_array_equal(
             shapes_data[0],
             [[5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8]]
             )
     assert len(shapes_data) == 1
-    assert "features" in layer_kwargs
-    assert type(layer_kwargs["features"]) is pd.DataFrame
+    assert 'features' in layer_kwargs
+    assert type(layer_kwargs['features']) is pd.DataFrame
 
 
 def test_get_skeleton_horse():
@@ -40,8 +40,8 @@ def test_get_skeleton_horse():
             labels_layer, skeleton_type
             )
     assert len(shapes_data) == 24  # 24 line segments in the horse skeleton
-    assert "features" in layer_kwargs
-    assert type(layer_kwargs["features"]) is pd.DataFrame
+    assert 'features' in layer_kwargs
+    assert type(layer_kwargs['features']) is pd.DataFrame
 
 
 def test_gui(make_napari_viewer):
@@ -54,10 +54,10 @@ def test_gui(make_napari_viewer):
     (skel_layer,) = viewer._add_layer_from_data(*ldt)
 
     dw, widget = viewer.window.add_plugin_dock_widget(
-            "skan", "Color Skeleton Widget"
+            'skan', 'Color Skeleton Widget'
             )
-    widget.feature_name.value = "euclidean_distance"
+    widget.feature_name.value = 'euclidean_distance'
     widget()
     layer = viewer.layers[-1]
-    assert layer.edge_colormap.name == "viridis"
+    assert layer.edge_colormap.name == 'viridis'
     assert len(layer.data) == 24

src/skan/test/test_skeleton_class.py
@@ -43,8 +43,8 @@ def test_skeleton_coordinates():
 
 
 @pytest.mark.skipif(
-        sys.platform.startswith("win"),
-        reason="caching not working on Windows CI for mysterious reasons",
+        sys.platform.startswith('win'),
+        reason='caching not working on Windows CI for mysterious reasons',
         )
 def test_path_length_caching():
     skeleton = Skeleton(skeleton3d)
@@ -100,9 +100,11 @@ def test_skeleton_summarize():
     image[skeleton2] = 1 + np.random.random(np.sum(skeleton2))
     skeleton = Skeleton(image)
     summary = summarize(skeleton)
-    assert set(summary["skeleton_id"]) == {0, 1}
-    assert np.all(summary["mean_pixel_value"] < 2
-                  ) and np.all(summary["mean_pixel_value"] > 1)
+    assert set(summary['skeleton_id']) == {0, 1}
+    assert (
+            np.all(summary['mean_pixel_value'] < 2)
+            and np.all(summary['mean_pixel_value'] > 1)
+            )
 
 
 def test_skeleton_label_image_strict():
@@ -124,7 +126,7 @@ def test_skeleton_label_image_strict():
             [0, 3, 0, 0, 0],
             [0, 3, 0, 0, 0],
             ])
-    non_junction_pixels = skeleton.degrees < 3
+    non_junction_pixels = (skeleton.degrees < 3)
     non_junction_coords = tuple(
             np.transpose(skeleton.coordinates[non_junction_pixels])
             )

src/skan/test/test_summary_utils.py
@@ -11,10 +11,10 @@ def test_find_main():
     non_main_edge_start = [2, 1]
     non_main_edge_finish = [3, 3]
 
-    non_main_df = summary_df.loc[summary_df["main"] == False]
+    non_main_df = summary_df.loc[summary_df['main'] == False]
     assert non_main_df.shape[0] == 1
     coords = non_main_df[[
-            "coord_src_0", "coord_src_1", "coord_dst_0", "coord_dst_1"
+            'coord_src_0', 'coord_src_1', 'coord_dst_0', 'coord_dst_1'
             ]].to_numpy()
     assert np.all(
             coords == non_main_edge_start + non_main_edge_finish