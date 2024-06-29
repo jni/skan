1107974f8076b084eed9235c7fa72da6f8301416
Juan Nunez-Iglesias <jni@fastmail.com>
2024-06-29 12:53:42 +1000

    Remove row_stack and use concatenate

--- src/skan/_testdata.py
+++ src/skan/_testdata.py
@@ -25,8 +25,8 @@ skeleton1 = np.array([[0, 1, 1, 1, 1, 1, 0],
 
 
 _zeros1 = np.zeros_like(skeleton1)
-skeleton2 = np.column_stack((skeleton1, _zeros1))
-skeleton2 = np.row_stack((skeleton2, skeleton2[:, ::-1]))
+skeleton2 = np.concatenate((skeleton1, _zeros1), axis=1)
+skeleton2 = np.concatenate((skeleton2, skeleton2[:, ::-1]), axis=0)
 
 skeleton3d = np.array([[[1, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0],
